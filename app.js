fd25b7cbd60f7e74fafa0f508b1d506bfc170f16
ObiwanBox <connorknabe@gmail.com>
2017-07-14 16:32:35 -0400

    logging for debugging

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var request = require('request');
+// var request = require('request');
 var nodemailer = require('nodemailer');
 var express = require('express');
-var retry = require('retry');
 var twilio = require('twilio');
 var loginInfo = require('./settings/loginInfo.js');
 var config = require('./settings/servicesConfig.js');
 var client = twilio(loginInfo.TWILIO_ACCOUNT_SID, loginInfo.TWILIO_AUTH_TOKEN);
 var app = express();
+var dns = require('dns');
+var request = require('requestretry');
+
 
 var services = config.services;
-var retryConfig = config.retry;
 var alertGroup = config.alertGroup;
+var retryAttempts = config.retryAttempts;
 
 var port = process.env.PORT || 2615;
 var router = express.Router();
@@ -22,18 +24,25 @@ app.get('/', function(req,res){
 
 checkForValidAlerts(alertGroup);
 populateServicesObj();
-var intervalTime = 0.1;
+var intervalTime = config.intervalTime;
 if(config.debugMode){
 	intervalTime = config.debugIntervalTime;
+	retryAttempts = config.debugRetryAttempts;
+	console.log('_____________________________\nIn debug mode will not send any alerts!!\n_____________________________');
 }
+
+
 
 setInterval(function(){
     for (var i=0;i<services.length;i++){
         var healthCheckEndPoint = services[i].route ? services[i].route : config.healthCheckEndpoint;
         var port = services[i].port ? ':'+services[i].port : '';
-        checkServiceHealth(services[i].name,services[i].ip+port+healthCheckEndPoint);
+        console.log('about to check health', new Date());
+		checkServiceHealth(services[i].name,services[i].ip+port+healthCheckEndPoint);
     }
 }, intervalTime*60*1000);
+
+
 
 function serviceObjectFromName(serviceName){
     var found = services.filter(function(item) { return item.name === serviceName; });
@@ -99,43 +108,90 @@ function sendEmail(alertInfo, msgContent){
 }
 
 function sendAlert(serviceObj, isOnline){
+	
+    console.log('send is online',isOnline,' alert', new Date());
+
     var serviceName = serviceObj.name;
-        if(isOnline){
-            sendMessage(serviceObj.alertInfo,serviceName+' is online!');
-            serviceObj.needsToSend = true;
-        } else if(serviceObj.needsToSend) {
-            sendMessage(serviceObj.alertInfo,serviceName+' is offline!');
-            serviceObj.needsToSend = false;
-        }
+    var isServiceOnServerInternet = serviceObj.ip == config.serverInternetIp;
+  	console.log('\n\n\n\n\n\n\n\n\n\n\n------------------------------------\n\n\n\n\n');
+  	console.log('Date', new Date());
+    console.log('serviceObj.ip',serviceObj.ip);
+    console.log('services.serverInternetIp',config.serverInternetIp);
+
+	var serverInternetObj = serviceObjectFromName(config.serverInternetName);
+	var internetHealthServer = serviceName == config.serverInternetName;
+	console.log('internet config service name',config.serverInternetName)
+	console.log('service name',serviceName)
+
+	console.log('isServiceOnServerInternet',isServiceOnServerInternet);
+    console.log('is checking server internet', internetHealthServer, new Date());
+    
+    if(internetHealthServer && isServiceOnServerInternet)
+    	console.log('\n\n\n\n\n\n\n\n\n\n\n_____________________\n\n\n\n\n');
+    	
+	if(isServiceOnServerInternet && internetHealthServer){
+		console.log('1',serviceName);
+		verifyOnlineAndSendMessage(serviceObj,serviceName,isOnline)
+	} else if(isServiceOnServerInternet && !internetHealthServer && serverInternetObj.needsToSend){
+		console.log('2',serviceName);
+		verifyOnlineAndSendMessage(serviceObj,serviceName,isOnline)
+	} else if(!isServiceOnServerInternet){
+		console.log('3',serviceName);
+
+		verifyOnlineAndSendMessage(serviceObj,serviceName,isOnline)
+	}            
 }
 
-function retryRequest(name, ip, cb ){
-    var operation = retry.operation(retryConfig);
-    operation.attempt(function(currentAttempt) {
-        request(ip, function (error, response, body) {
-            var statusCode = response && response.statusCode ? response.statusCode : null;
-            if(statusCode!=200){
-                error = new Error('Invalid route for '+ name);
-            }
-            if(operation.retry(error)){
-                return;
-            }
-            cb(error ? operation.mainError() : null, name, ip);
-        });
-    });
+function verifyOnlineAndSendMessage(serviceObj,serviceName, isOnline){
+	if(isOnline && !serviceObj.needsToSend){
+        sendMessage(serviceObj.alertInfo,serviceName+' is online!');
+        serviceObj.needsToSend = true;
+    } else if(!isOnline && serviceObj.needsToSend) {
+        sendMessage(serviceObj.alertInfo,serviceName+' is offline!');
+        serviceObj.needsToSend = false;
+    }	
 }
+
+function myRetryStrategy(err, response, body){
+  // retry the request if we had an error or if the response was a 'Bad Gateway'
+  return err || response.statusCode === 502;
+}
+
 
 function checkServiceHealth(name,ip){
-    retryRequest(name,ip, function(err, name, ip){
-        var serviceObj = serviceObjectFromName(name);
-        if(err){
-            serviceObj.isOnline = false;
-            sendAlert(serviceObj,serviceObj.isOnline);
-        } else if (!serviceObj.isOnline) {
-			serviceObj.isOnline = true;
-            sendAlert(serviceObj,serviceObj.isOnline);
-        }
-    });
+	var serviceObj = serviceObjectFromName(name);
+	console.log('trying',ip, new Date());
+	request({
+		timeout:3000,
+	    url: ip,
+		retryDelay: 2000,
+	    json:true,
+	    retryStrategy: myRetryStrategy,
+		maxAttempts: retryAttempts	    
+	})
+	.then(function (response) {
+
+		console.log('success', name);
+		
+		if(response.attempts >= retryAttempts){
+			console.log("Max attempts hit", new Date());
+			serviceObj.isOnline = false;
+			setTimeout(function(){
+				sendAlert(serviceObj,serviceObj.isOnline);
+			}, 15000);
+		}
+		serviceObj.isOnline = true;
+		setTimeout(function(){
+			sendAlert(serviceObj,serviceObj.isOnline);
+		}, 15000);
+	})
+	.catch(function(error) {
+		console.log('err',error);
+        serviceObj.isOnline = false;
+		setTimeout(function(){
+			sendAlert(serviceObj,serviceObj.isOnline);
+		}, 15000);
+	});
 }
 
 function populateServicesObj(){
@@ -159,3 +215,4 @@ function checkForValidAlerts(alertGroup){
         }
     }
 }
+
